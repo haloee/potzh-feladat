1efdbd9b6e8f6ffc27de2c32befacb1b3740378b
Sztaracsek Matyas <sztaracsekmatyas@gmail.com>
2025-05-13 15:31:05 +0200

    MovieService

---
+++ src/main/java/service/MovieService.java
@@ -0,0 +1,13 @@
+package service;
+import hu.pte.mik.prog4.potzh.entity.MovieEntity;
+import Repository.Repository;
+import java.util.List;
+import hu.pte.mik.generated.client.MovieDataService;
+import hu.pte.mik.generated.client.MovieDataService_Service;
+import hu.pte.mik.generated.client.MovieDataRequest;
+import hu.pte.mik.generated.client.MovieDataResponse;
+public class MovieService {
+
+
+
+}
